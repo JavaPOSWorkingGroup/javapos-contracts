b609461a3c2c65d0e5fe66d360bb0efb37b00c5a
mjpcger <37701263+mjpcger@users.noreply.github.com>
2023-09-28 20:43:25 +0200

    Interface Additions and Header Changes
    Interfaces for devices that have not been changed in UPOS 1.16 have been added.
    Licence headers of the other 1.16 interfaces have been changed to match the Javapos conditions.

--- src/main/java/jpos/services/SoundPlayerService116.java
+++ src/main/java/jpos/services/SoundPlayerService116.java
@@ -1,26 +1,38 @@
-/*
- * Copyright 2023 Martin Conrad
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+//////////////////////////////////////////////////////////////////////
+//
+// The JavaPOS library source code is now under the CPL license, which 
+// is an OSS Apache-like license. The complete license is located at:
+//    http://www.ibm.com/developerworks/library/os-cpl.html
+//
+//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//
+// This software is provided "AS IS".  The JavaPOS working group (including
+// each of the Corporate members, contributors and individuals)  MAKES NO
+// REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NON-INFRINGEMENT. The JavaPOS working group shall not be liable for
+// any damages suffered as a result of using, modifying or distributing this
+// software or its derivatives.Permission to use, copy, modify, and distribute
+// the software and its documentation for any purpose is hereby granted.
+//
+// SoundPlayerService116
+//
+//   Interface defining all new capabilities, properties and methods
+//   that are specific to Sound Player for release 1.16.
+//
+//   Generated from SoundPlayerControl116.
+//
+/////////////////////////////////////////////////////////////////////
 
 package jpos.services;
 
 import jpos.JposException;
 import jpos.loader.JposServiceInstance;
 
-public interface SoundPlayerService116 extends BaseService, JposServiceInstance {
+public interface SoundPlayerService116 extends BaseService, JposServiceInstance
+{
     public boolean getCapCompareFirmwareVersion() throws JposException;
 
     public int getCapPowerReporting() throws JposException;
